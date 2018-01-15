9bd40d534ba36dfb06ae81ce13634387c1a10ee4
xieguangzeng <smsxgz@gmail.com>
2018-01-15 11:26:10 +0800

    no copy

---
+++ gym_classic_control/config/double_dqn.wider_network.target_no_copy/dqn.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+
+
+def dqn(sess,
+        env,
+        update_fn,
+        q_estimator,
+        memory_buffer,
+        summary_writer,
+        exploration_policy_fn,
+        num_iterations=500000):
+
+    total_t = sess.run(tf.train.get_global_step())
+    states = env.reset()
+    for i in range(num_iterations):
+        try:
+            q_values = q_estimator.predict(sess, states)
+            actions = exploration_policy_fn(q_values, total_t)
+            next_states, rewards, dones, info = env.step(actions)
+
+            memory_buffer.extend(
+                zip(states, actions, rewards, next_states, dones))
+            sample = memory_buffer.sample()
+            if sample:
+                total_t = update_fn(*sample)
+
+            states = next_states
+
+            # Add summaries to tensorboard
+            if info:
+                mean_reward = sum(
+                    list(item.values())[0] for item in info) / len(info)
+                episode_summary = tf.Summary()
+                episode_summary.value.add(
+                    simple_value=mean_reward,
+                    node_name="rewards",
+                    tag="rewards")
+
+                summary_writer.add_summary(episode_summary, total_t)
+                summary_writer.flush()
+        except KeyboardInterrupt:
+            break
+
+    env.close()
---
+++ gym_classic_control/config/double_dqn.wider_network.target_no_copy/estimator.py
@@ -0,0 +1,158 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+
+
+class Estimator(object):
+    """Q-Value Estimator neural network.
+
+    This network is used for both the Q-Network and the Target Network.
+    """
+
+    def __init__(self,
+                 network,
+                 optimizer=None,
+                 summary_writer=None,
+                 scope="estimator"):
+        """
+        Notes:
+            network is callable,
+            receives nothing, returns X's placeholder and last layer of model.
+        """
+        self.network = network
+        self.scope = scope
+        self.optimizer = optimizer
+        self.summary_writer = summary_writer
+        self._build_model()
+
+    def _build_model(self):
+        """Builds the Tensorflow graph."""
+        with tf.variable_scope(self.scope):
+            self.actions_pl = tf.placeholder(
+                shape=[None], dtype=tf.int32, name="actions")
+            self.y_pl = tf.placeholder(
+                shape=[None], dtype=tf.float32, name="y")
+
+            self.X_pl, self.predictions = self.network()
+
+            batch_size = tf.shape(self.X_pl)[0]
+
+            # Get the predictions for the chosen actions only
+            gather_indices = tf.range(batch_size) * tf.shape(
+                self.predictions)[1] + self.actions_pl
+            self.action_predictions = tf.gather(
+                tf.reshape(self.predictions, [-1]), gather_indices)
+
+            # Calcualte the loss
+            self.losses = tf.squared_difference(self.y_pl,
+                                                self.action_predictions)
+            self.loss = tf.reduce_mean(self.losses)
+
+            if self.optimizer:
+                self.train_op = self.optimizer.minimize(
+                    self.loss, global_step=tf.train.get_global_step())
+
+            if self.summary_writer:
+                self.summaries = tf.summary.merge([
+                    tf.summary.scalar("loss", self.loss),
+                    tf.summary.histogram("loss_hist", self.losses),
+                    tf.summary.histogram("q_values_hist", self.predictions),
+                    tf.summary.scalar("max_q_value",
+                                      tf.reduce_max(self.predictions)),
+                    tf.summary.scalar("min_q_value",
+                                      tf.reduce_min(self.predictions))
+                ])
+
+    def predict(self, sess, s):
+        return sess.run(self.predictions, {self.X_pl: s})
+
+    def update(self, sess, s, a, y):
+        feed_dict = {self.X_pl: s, self.y_pl: y, self.actions_pl: a}
+        summaries, global_step, _, loss = sess.run([
+            self.summaries,
+            tf.train.get_global_step(), self.train_op, self.loss
+        ], feed_dict)
+        if self.summary_writer:
+            self.summary_writer.add_summary(summaries, global_step)
+        return loss, global_step
+
+
+def update_target(sess, q, target, rho):
+    e2_params = [
+        t for t in tf.trainable_variables() if t.name.startswith(target.scope)
+    ]
+    e2_params = sorted(e2_params, key=lambda v: v.name)
+    name_list = [v.name.replace(target.scope, q.scope) for v in e2_params]
+
+    e1_params = [t for t in tf.trainable_variables() if t.name in name_list]
+    e1_params = sorted(e1_params, key=lambda v: v.name)
+
+    update_ops = []
+    for e1_v, e2_v in zip(e1_params, e2_params):
+        op = e2_v.assign((e1_v - e2_v) * rho + e2_v)
+        update_ops.append(op)
+
+    sess.run(update_ops)
+
+
+class Update(object):
+    def __init__(self,
+                 sess,
+                 checkpoint_path,
+                 q_estimator,
+                 target_estimator,
+                 discount_factor=0.99,
+                 update_target_rho=0.01,
+                 save_model_every=100):
+
+        self.q_estimator = q_estimator
+        self.target_estimator = target_estimator
+        self.sess = sess
+        self.checkpoint_path = checkpoint_path
+        self.discount_factor = discount_factor
+        self.update_target_rho = update_target_rho
+        self.save_model_every = save_model_every
+
+        self.tot = 0
+        self.saver = tf.train.Saver()
+
+        latest_checkpoint = tf.train.latest_checkpoint(checkpoint_path)
+        if latest_checkpoint:
+            print("Loading model checkpoint {}...".format(latest_checkpoint))
+            try:
+                self.saver.restore(sess, latest_checkpoint)
+            except Exception:
+                print('Loading failed, we will Start from scratch!!')
+
+    def __call__(self, *args):
+        loss, global_step = self._update(*args)
+        self.tot += 1
+        print('\r{}th update, loss: {}'.format(self.tot, loss), end='')
+        sys.stdout.flush()
+
+        update_target(self.sess, self.q_estimator, self.target_estimator,
+                      self.update_target_rho)
+
+        if self.tot % self.save_model_every == 0:
+            self.saver.save(self.sess,
+                            os.path.join(self.checkpoint_path, 'model'),
+                            global_step)
+            print("\nSave session.")
+
+        return global_step
+
+    def _update(self, states_batch, action_batch, reward_batch,
+                next_states_batch, done_batch):
+        batch_size = states_batch.shape[0]
+        best_actions = np.argmax(
+            self.q_estimator.predict(self.sess, next_states_batch), axis=1)
+        q_values_next_target = self.target_estimator.predict(
+            self.sess, next_states_batch)
+        discount_factor = np.invert(done_batch).astype(
+            np.float32) * self.discount_factor
+        targets_batch = reward_batch + discount_factor * \
+            q_values_next_target[np.arange(batch_size), best_actions]
+
+        return self.q_estimator.update(self.sess, states_batch, action_batch,
+                                       targets_batch)
