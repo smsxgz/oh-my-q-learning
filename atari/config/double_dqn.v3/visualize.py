4b4515077bfa1b288de0affea0654491dfe8d443
xieguangzeng <smsxgz@gmail.com>
2018-03-05 14:40:56 +0800

    fix small bugs

--- atari/config/double_dqn.v3/visualize.py
+++ atari/config/double_dqn.v3/visualize.py
@@ -12,13 +12,16 @@ from estimator import Estimator
 @click.option('--game_name')
 def visualize(game_name):
     videoWriter = cv2.VideoWriter('{}.mp4'.format(game_name),
-                                  cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), 60,
-                                  (210, 160))
+                                  cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
+                                  60, (210, 160))
     env = atari_env(game_name, videowriter=videoWriter)
 
     checkpoint_path = os.path.join(train_path, game_name, 'models')
     estimator = Estimator(
-        env.state_shape, env.action_n, 1e-4, update_target_rho=1.0)
+        env.observation_space.shape,
+        env.action_space.n,
+        1e-4,
+        update_target_rho=1.0)
 
     config = tf.ConfigProto(allow_soft_placement=True)
     config.gpu_options.allow_growth = True
