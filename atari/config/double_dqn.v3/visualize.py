5fb607522f21082e6ce2d6fcd707a19f5319ded6
xieguangzeng <smsxgz@gmail.com>
2018-03-05 14:30:31 +0800

    ifmain

--- atari/config/double_dqn.v3/visualize.py
+++ atari/config/double_dqn.v3/visualize.py
@@ -1,18 +1,22 @@
+import os
 import cv2
 import click
 import numpy as np
 import tensorflow as tf
+from util import train_path
 from wrapper import atari_env
 from estimator import Estimator
 
 
 @click.command()
 @click.option('--game_name')
-def visualize(game_name, checkpoint_path):
+def visualize(game_name):
     videoWriter = cv2.VideoWriter('{}.mp4'.format(game_name),
                                   cv2.cv.CV_FOURCC('M', 'J', 'P', 'G'), 60,
                                   (210, 160))
     env = atari_env(game_name, videowriter=videoWriter)
+
+    checkpoint_path = os.path.join(train_path, game_name, 'models')
     estimator = Estimator(
         env.state_shape, env.action_n, 1e-4, update_target_rho=1.0)
 
@@ -32,3 +36,7 @@ def visualize(game_name, checkpoint_path):
         state, reward, done, _ = env.step(action)
         if env.unwrapped.ale.lives() == 0:
             break
+
+
+if __name__ == '__main__':
+    visualize()
