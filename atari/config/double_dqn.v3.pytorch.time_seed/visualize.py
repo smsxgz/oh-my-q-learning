4678a27547ef7b9d146681ca75a4260d0244314f
smsxgz <smsxgz@gmail.com>
2018-03-12 21:21:16 +0800

    time seed

---
+++ atari/config/double_dqn.v3.pytorch.time_seed/visualize.py
@@ -0,0 +1,81 @@
+import os
+import click
+import imageio
+import numpy as np
+from tqdm import tqdm
+
+from util import train_path
+from wrapper import atari_env
+from estimator import Estimator
+
+
+@click.command()
+@click.option('--game_name')
+@click.option('--write_video', is_flag=True)
+def main(game_name, write_video):
+    env = atari_env(game_name)
+
+    estimator = Estimator(
+        env.observation_space.shape,
+        env.action_space.n,
+        1e-4,
+        update_target_rho=1.0)
+
+    checkpoint_path = os.path.join(train_path, game_name, 'models')
+    estimator.restore(checkpoint_path)
+
+    def visualize():
+        total_t = estimator.get_global_step()
+        videoWriter = imageio.get_writer(
+            'train_log/{}-{}.mp4'.format(game_name, total_t), fps=30)
+
+        state = env.reset(videowriter=videoWriter)
+        lives = env.unwrapped.ale.lives()
+        print(lives)
+        r = 0
+        tot = 0
+        while True:
+            q_value = estimator.predict(np.array([state]))
+            action = np.argmax(q_value[0])
+            state, reward, done, info = env.step(action)
+            r += reward
+            tot += 1
+            if done:
+                lives = env.unwrapped.ale.lives()
+                print(lives)
+                if info['was_real_done']:
+                    print(tot, r)
+                    break
+                else:
+                    state = env.reset()
+        videoWriter.close()
+
+    def evaluate(num_eval=50):
+        res = []
+        for i in tqdm(range(num_eval)):
+            env.seed()
+            state = env.reset()
+            r = 0
+            while True:
+                q_value = estimator.predict(np.array([state]))
+                action = np.argmax(q_value[0])
+                state, reward, done, info = env.step(action)
+                r += reward
+                if done:
+                    if info['was_real_done']:
+                        res.append(r)
+                        break
+                    else:
+                        state = env.reset()
+        print('mean: {}, max: {}'.format(sum(res) / num_eval, max(res)))
+
+    if write_video:
+        print('Writing video ...')
+        visualize()
+    else:
+        print('Evaluating ...')
+        evaluate()
+
+
+if __name__ == '__main__':
+    main()
