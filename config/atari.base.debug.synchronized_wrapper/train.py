85a6f7e5f9d2661dd1d5e18096e5ccd9c6911200
smsxgz <smsxgz@megvii.com>
2018-08-17 20:12:26 +0800

    synchronized wrapper

---
+++ config/atari.base.debug.synchronized_wrapper/train.py
@@ -0,0 +1,49 @@
+import os
+import click
+import traceback
+from dqn import dqn
+from synchronized_wrapper.atari_wrapper import get_atari_env_fn
+from synchronized_wrapper.agent import Agent
+from estimator import get_estimator
+from util import train_path
+
+
+@click.command()
+@click.option('--game_name', prompt='game name:')
+@click.option('--lr', type=float, default=0.0001)
+@click.option('--num_agents', type=int, default=32)
+@click.option('--update_target_every', type=int, default=1000)
+@click.option('--model_name', default='dqn')
+def main(game_name, lr, num_agents, update_target_every, model_name):
+    assert 'NoFrameskip-v4' in game_name
+
+    basename = '{}:lr={}:na={}:ute={}:{}'.format(
+        game_name[:-14], lr, num_agents, update_target_every, model_name)
+
+    # env = Agent(num_agents, get_atari_env_fn(game_name), basename, backend='multiprocessing')
+    env = Agent(num_agents, get_atari_env_fn(game_name), basename, backend='ray')
+    try:
+        action_n = int(env.action_space.nvec[0])
+        estimator = get_estimator(model_name, action_n, lr, 0.99)
+        base_path = os.path.join(train_path, basename)
+        print("start training!!")
+        dqn(env,
+            estimator,
+            base_path,
+            batch_size=32,
+            epsilon=0.01,
+            save_model_every=1000,
+            update_target_every=update_target_every,
+            learning_starts=200,
+            memory_size=100000,
+            num_iterations=40000000)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt!!")
+    except Exception:
+        traceback.print_exc()
+    finally:
+        env.close()
+
+
+if __name__ == "__main__":
+    main()
